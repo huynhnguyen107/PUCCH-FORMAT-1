59dd105c278b0e1113893e4e445ddd60f5d43a2e
nvhuynh107@gmail.com <nvhuynh107@gmail.com>
2026-02-18 20:38:29 -0500

    re-start

---
+++ uci_dmrs_gen/uci_dmrs_gen.srcs/sources_1/new/uci_dmrs_gen.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/18/2026 07:02:52 PM
+// Design Name: 
+// Module Name: uci_dmrs_gen
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module uci_dmrs_gen(
+
+    );
+endmodule
